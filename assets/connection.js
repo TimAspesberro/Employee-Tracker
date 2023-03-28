2d2f943751070f9eb00d661e38a2f9e37b01e347
Tim Aspesberro <taspes@gmail.com>
2023-03-27 21:31:17 -0400

    added to server.js, worked on inquirer

--- assets/connection.js
+++ assets/connection.js
@@ -31,7 +31,7 @@ const viewEmployees = () => {
       });
 };
 
-const addDept = () => {
+const addDeptartment = () => {
 
 };
 
--- server.js
+++ server.js
@@ -1,19 +1,67 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql');
 
-const questions = [
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_DB',
+});
+
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    } else {
+        startApp();
+    }
+});
+
+
+const question = [
     {
         name: 'viewDept',
+        type: 'list',
         message: 'Select an option.',
-        choices: ['view all departments', 'view all roles', 'view all employees'],
-        type: 'choices',
+        choices: [
+        'view all departments', 
+        'view all roles', 
+        'view all employees',
+        'add a department',
+        'add a role',
+        'add an employee',
+        'update an employee role'
+        ],
     },
 ];
 
-function init () {
+function startApp () {
     inquirer
-        .prompt(questions)
-        .then(() => {
-
+        .prompt(question)
+        .then((response) => {
+            switch (response.action) {
+                case "View all departments":
+                  viewDept();
+                break;
+                case "View all roles":
+                  viewRoles();
+                break;
+                case "View all employees":
+                  viewEmployees();
+                break;
+                case "add a department":
+                  addDeptartment();
+                break;
+                case "add a role":
+                  addRole();
+                break;
+                case "add an employee":
+                  addEmployee();
+                break;
+                case "update an employee role":
+                  updateEmployeeRole();
+                break;
+            }
         })
         .catch((error) => {
             console.log(error);
